60164f4285eb510c69c1806fbdfec83d26519d6a
Sai Rakshith <sai@appmastery.co>
2023-08-15 17:53:23 +0530

    add/added-OAI

--- lib/simple-s3-cloud_front-oai-stack.ts
+++ lib/simple-s3-cloud_front-oai-stack.ts
@@ -1,16 +1,63 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// Imports.
+import { Construct } from "constructs";
+import {
+  Stack,
+  StackProps,
+  aws_s3 as s3,
+  RemovalPolicy,
+  aws_cloudfront as cloudfront,
+  aws_cloudfront_origins as origins,
+} from "aws-cdk-lib";
 
-export class SimpleS3CloudFrontOaiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export interface SimpleS3CloudFrontOaiStackProps extends StackProps {
+  deployment_environment: string;
+}
+
+export class SimpleS3CloudFrontOaiStack extends Stack {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: SimpleS3CloudFrontOaiStackProps
+  ) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleS3CloudFrontOaiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const applicationName = "SimpleS3CloudFrontOAIStack";
+    const deployment_environment = props.deployment_environment;
+    // Creating an S3 Bucket.
+    const s3_bucket = new s3.Bucket(
+      this,
+      `s3-${applicationName}-${deployment_environment}`,
+      {
+        bucketName: `s3-simples3cloudfrontoaistack-${deployment_environment}`,
+        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+        removalPolicy: RemovalPolicy.DESTROY,
+        autoDeleteObjects: true,
+      }
+    );
+    // Creating an Origin Access Identity
+    const oai = new cloudfront.OriginAccessIdentity(
+      this,
+      `oai-${applicationName}-${deployment_environment}`,
+      {
+        comment: "OAI",
+      }
+    );
+    // Grant Origin Access Identity Access to S3 Bucket.
+    s3_bucket.grantRead(oai);
+    // Creating a Cloudfront Distribution
+    const cloudfront_distribution = new cloudfront.Distribution(
+      this,
+      `cf-${applicationName}-${deployment_environment}`,
+      {
+        defaultBehavior: {
+          origin: new origins.S3Origin(s3_bucket, {
+            originAccessIdentity: oai,
+          }),
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+          compress: true,
+        },
+      }
+    );
   }
 }
