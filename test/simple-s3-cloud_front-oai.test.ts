3ea5201b2259b31121d366e0b567e3fb218fb998
Sai Rakshith <sai@appmastery.co>
2023-08-15 17:39:44 +0530

    Initial commit

---
+++ test/simple-s3-cloud_front-oai.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SimpleS3CloudFrontOai from '../lib/simple-s3-cloud_front-oai-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/simple-s3-cloud_front-oai-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SimpleS3CloudFrontOai.SimpleS3CloudFrontOaiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
